ea82396b5e0786ce0a52d04937efbd0c634a14ad
lin <275762580@qq.com>
2015-01-25 15:29:56 +0800

    init

---
+++ app/src/main/java/com/android/launcher3/ToggleWeightWatcher.java
@@ -0,0 +1,17 @@
+package com.android.launcher3;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class ToggleWeightWatcher extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(this, Launcher.class));
+
+        finish();
+    }
+}
